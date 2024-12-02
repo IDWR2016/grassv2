73b37040128b6c01a314d9cac86c70fb0727f7ee
IDWR <35910302+IDWR2016@users.noreply.github.com>
2024-12-02 18:49:40 +0700

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,8 +1,10 @@
 class Config {
   constructor() {
-    this.ipCheckURL = 'https://ipinfo.io/json';
-    this.wssHost = 'proxy2.wynd.network:4444';
-    this.retryInterval = 20000;
+    this.ipCheckURL = "https://ipinfo.io/json";
+    this.wssList = ["proxy2.wynd.network:4444", "proxy2.wynd.network:4650"];
+    this.retryInterval = 500;
+    this.wssHost =
+      this.wssList[Math.floor(Math.random() * this.wssList.length)];
   }
 }
 
